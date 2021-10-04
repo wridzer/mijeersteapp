0c8e70cb7909ee68f6d67ffc7d07d1d3c09314fd
Wridzer Kamphuis <wridzer12@gmail.com>
2021-10-04 16:06:39 +0200

    added colors and progressbars

--- MijnEersteApp/MijnEersteApp/MainPage.xaml.cs
+++ MijnEersteApp/MijnEersteApp/MainPage.xaml.cs
@@ -19,25 +19,32 @@ namespace MijnEersteApp
         private static float social = 50;
         private static List<float> stats = new List<float>() { food, drink, attention, rest, social };
 
-        //lowering stats
+        //control stats
         private float lowerStatIntervan = 5000;
-        private float LowerStatAmount = 5;
+        private static float lowerStatAmount = 5;
+        private float addStatAmount = 5;
 
         public MainPage()
         {
             InitializeComponent();
             System.Timers.Timer removeStatTimer = new System.Timers.Timer();
+            System.Timers.Timer updateTimer = new System.Timers.Timer();
+            App.Current.Resources["LabelColor"] = MoodColor(250);
+
+            //Timers
             removeStatTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
             removeStatTimer.Interval = lowerStatIntervan;
             removeStatTimer.Enabled = true;
+            updateTimer.Elapsed += new ElapsedEventHandler(UpdateStats);
+            updateTimer.Interval = 100;
+            updateTimer.Enabled = true;
         }
 
-        public float TotalMood()
+        public static Xamarin.Forms.Color MoodColor(float mood)
         {
-            float color;
-            float mood = food + drink + attention + rest + social;
-            if (mood > 150) color = 000000;
-            else color = 111111;
+            Xamarin.Forms.Color color;
+            if (mood > 230) color = Color.FromHex("#1ccf00");
+            else color = Color.FromHex("#cf0000");
             return color;
         }
 
@@ -45,34 +52,80 @@ namespace MijnEersteApp
         {
             for (int i = 0; i < stats.Count; i++)
             {
-                stats[i] -= 5;
+                stats[i] -= lowerStatAmount;
+                if(stats[i] < 0)
+                {
+                    stats[i] = 0;
+                }
                 Console.WriteLine(stats[i]);
             }
         }
 
+        private static void UpdateStats(object source, ElapsedEventArgs e)
+        {
+            App.Current.Resources["FillFood"] = stats[0] / 100 * 20;
+            App.Current.Resources["FillColorFood"] = MoodColor(stats[0]);
+            
+            App.Current.Resources["FillDrink"] = stats[1] / 100 * 20;
+            App.Current.Resources["FillColorDrink"] = MoodColor(stats[1]);
+            
+            App.Current.Resources["FillSleep"] = stats[2] / 100 * 20;
+            App.Current.Resources["FillColorSleep"] = MoodColor(stats[2]);
+            
+            App.Current.Resources["FillPlay"] = stats[3] / 100 * 20;
+            App.Current.Resources["FillColorPlay"] = MoodColor(stats[3]);
+            
+            App.Current.Resources["FillSocial"] = stats[4] / 100 * 20;
+            App.Current.Resources["FillColorSocial"] = MoodColor(stats[4]);
+
+            float mood = stats.Take(5).Sum();
+            App.Current.Resources["LabelColor"] = MoodColor(mood);
+        }
+
+
         void Food_Clicked(object sender, System.EventArgs e)
         {
-            stats[0]++;
+            stats[0] += addStatAmount;
+            if (stats[0] > 100)
+            {
+                stats[0] = 100;
+            }
             ((Button)sender).Text = $"Food: {stats[0]}";
         }
         void Drink_Clicked(object sender, System.EventArgs e)
         {
-            stats[1]++;
+            stats[1] += addStatAmount;
+            if (stats[1] > 100)
+            {
+                stats[1] = 100;
+            }
             ((Button)sender).Text = $"Drink: {stats[1]}";
         }
         void Sleep_Clicked(object sender, System.EventArgs e)
         {
-            stats[2]++;
+            stats[2] += addStatAmount;
+            if (stats[2] > 100)
+            {
+                stats[2] = 100;
+            }
             ((Button)sender).Text = $"Rest: {stats[2]}";
         }
         void Play_Clicked(object sender, System.EventArgs e)
         {
-            stats[3]++;
+            stats[3] += addStatAmount;
+            if (stats[3] > 100)
+            {
+                stats[3] = 100;
+            }
             ((Button)sender).Text = $"Attention: {stats[3]}";
         }
         void Social_Clicked(object sender, System.EventArgs e)
         {
-            stats[4]++;
+            stats[4] += addStatAmount;
+            if (stats[4] > 100)
+            {
+                stats[4] = 100;
+            }
             ((Button)sender).Text = $"Social: {stats[4]}";
         }
     }
